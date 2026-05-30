b3730d5a81eed967ce88ac691ae2c331cbb9a272
aevankow <69961371+aevankow@users.noreply.github.com>
2026-05-30 16:58:13 -0400

    started script

---
+++ Script_SpeciesRichness.R
@@ -0,0 +1,7 @@
+
+#This map was created using the tutorial by @joelnitta "Obtaining occurrence and phylogeny data in R"
+#https://github.com/joelnitta/spatial-phy-workshop/blob/main/tutorials/occ_phy.md
+ 
+#Please see this tutorial for setup and other detailed information
+
+
